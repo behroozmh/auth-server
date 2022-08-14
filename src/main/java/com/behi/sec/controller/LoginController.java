a3fea118786e34cd90050e91487ed3d708e117e2
behrooz.mh <behrooz.mh@gmail.com>
2022-08-14 09:51:03 +0430

    behrooz.mh: add login With Mobile pages

--- src/main/java/com/behi/sec/controller/LoginController.java
+++ src/main/java/com/behi/sec/controller/LoginController.java
@@ -1,21 +1,33 @@
 package com.behi.sec.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/login")
 public class LoginController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String getLogin(){
         return "login";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
     public String postLogin(){
         return "login";
+    }
+
+
+    @RequestMapping(value = "/lwm",method = RequestMethod.GET)
+    public String getLoginWithMobile(ModelAndView model){
+        return "loginWithMobile";
+    }
+
+    @RequestMapping(value = "/lwm",method = RequestMethod.POST)
+    public String postLoginWithMobile(ModelAndView model){
+        return "loginWithMobile";
     }
 
 
