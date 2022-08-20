e475de942b1455b739ff6b42e89b78b02c29ed2b
behrooz.mh <behrooz.mh@gmail.com>
2022-08-20 09:52:21 +0430

    behrooz.mh: new feat

--- src/main/java/com/behi/sec/controller/LoginController.java
+++ src/main/java/com/behi/sec/controller/LoginController.java
@@ -4,29 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String getLogin(){
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLogin() {
         return "login";
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String postLogin(){
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String postLogin() {
         return "login";
     }
 
 
-    @RequestMapping(value = "/lwm",method = RequestMethod.GET)
-    public String getLoginWithMobile(ModelAndView model){
+    @RequestMapping(value = "/lwm", method = RequestMethod.GET)
+    public String getLoginWithMobile(ModelMap model) {
         return "loginWithMobile";
     }
 
-    @RequestMapping(value = "/lwm",method = RequestMethod.POST)
-    public String postLoginWithMobile(ModelAndView model){
+    @RequestMapping(value = "/lwm", method = RequestMethod.POST)
+    public String postLoginWithMobile(ModelMap model) {
         return "loginWithMobile";
     }
 
